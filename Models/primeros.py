a283056c87ac3eaf0e56237b4242768dc08ef33f
Juan Diego Varon <jdiegov15@gmail.com>
2022-10-02 23:24:21 -0500

    format string para primeros

--- Models/primeros.py
+++ Models/primeros.py
@@ -1,8 +1,24 @@
 from typing import List
 
+
 class Primeros:
     noTerminal: str
     conjuntoNoTerminales: List[str]
 
-    def toString(self) -> str:
-        pass
+    def __init__(self, noTerminal:str, conjuntoNoTerminales:List[str]) -> None:
+        self.noTerminal = noTerminal
+        self.conjuntoNoTerminales = conjuntoNoTerminales
+
+    def cadenaNoTerminales(self) -> str:
+        cadena = ""
+        count = 0
+        for noTerminal in self.conjuntoNoTerminales:
+            if count == len(self.conjuntoNoTerminales):
+                cadena += noTerminal
+            else:
+                cadena += noTerminal + ", "
+        return cadena
+
+    def __str__(self) -> str:
+        texto = "prim({}) = {{}}"
+        return texto.format(self.noTerminal, self.cadenaNoTerminales)
